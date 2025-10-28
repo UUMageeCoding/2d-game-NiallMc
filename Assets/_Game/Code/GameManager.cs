4f4d7e5937124d5ea19b750e7d39a1581343fb9f
Niall McCann <Mc_Cann-N1@ulster.ac.uk>
2025-10-28 15:47:22 +0000

    w6

---
+++ Assets/_Game/Code/GameManager.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void sceneChange(String scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
