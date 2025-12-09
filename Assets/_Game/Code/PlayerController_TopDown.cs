0b82443c9dd6614ffa115466243a96fea9335ad8
Niall McCann <Mc_Cann-N1@ulster.ac.uk>
2025-12-09 12:11:09 +0000

    tues

--- Assets/_Game/Code/PlayerController_TopDown.cs
+++ Assets/_Game/Code/PlayerController_TopDown.cs
@@ -1,12 +1,14 @@
 using NUnit.Framework;
 using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController_TopDown : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
+    
     
     private Rigidbody2D rb;
     private Vector2 moveInput;
@@ -15,12 +17,17 @@ public class PlayerController_TopDown : MonoBehaviour
     Vector2 newPosition;
     public Transform Aim;
     bool isWalking = false;
-    bool knockback = false;
+    bool isUp = false;
+    bool isDown = false;
+
+    SpriteRenderer sr;
+    Animator an;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        an = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
     
     void Update()
@@ -35,11 +42,11 @@ public class PlayerController_TopDown : MonoBehaviour
             lastMoveDirection = moveInput;
             vector3 = Vector3.left * lastMoveDirection.x + Vector3.down * lastMoveDirection.y;
             Aim.rotation = quaternion.LookRotation(Vector3.forward, vector3);
+            
         }
         else if (moveX != 0 || moveY != 0)
         {
             isWalking = true;
-
         }
         // Get input
         moveInput.x = Input.GetAxisRaw("Horizontal");
@@ -57,7 +64,34 @@ public class PlayerController_TopDown : MonoBehaviour
         {
             vector3 = Vector3.left * moveInput.x + Vector3.down * moveInput.y;
             Aim.rotation = quaternion.LookRotation(Vector3.forward, vector3);
+            an.SetBool("IsWalking", true);
+            if (moveInput.y > 0)
+            {
+                an.SetBool("IsUp", true);
+                an.SetBool("IsDown", false);
+            }
+            else if (moveInput.y < 0)
+            {
+                an.SetBool("IsDown", true);
+                an.SetBool("IsUp", false);
+            }
+
+            if ((moveInput.x < 0) && (moveInput.y == 0))
+            {
+                sr.flipX = true;
+            }
+            else if ((moveInput.x > 0) && (moveInput.y == 0))
+            {
+                sr.flipX = false;
+            }
+        }
+        else if (isWalking == false)
+        {
+            an.SetBool("IsWalking", false);
+            an.SetBool("IsDown", false);
+            an.SetBool("IsUp", false);
         }
         rb.MovePosition(newPosition);
+
     }
 }
